217da13affaccf104e2f410f30146db51c81b031
Hella Snoek <hella.snoek@nikhef.nl>
2020-12-29 16:58:16 +0100

    minor change

--- 22 Opdrachten Module 2/20 Meesjes/M2.2_Meesjes.py
+++ 22 Opdrachten Module 2/20 Meesjes/M2.2_Meesjes.py
@@ -2,7 +2,6 @@
 # Date: Jan 2021
 # Code for Data Analyse en Statistiek course
 
-
 import DAS_DatasetGenerator as ds
 import numpy as np
 import matplotlib.pyplot as plt
--- 22 Opdrachten Module 2/30 HalfwaardedikteII/M2.3_Halfwaardedikte.py
+++ 22 Opdrachten Module 2/30 HalfwaardedikteII/M2.3_Halfwaardedikte.py
@@ -1,3 +1,7 @@
+# Author: Hella Snoek en ...
+# Date: Jan 2021
+# Code for Data Analyse en Statistiek course
+
 import DAS_DatasetGenerator as ds
 import numpy as np
 import matplotlib.pyplot as plt
